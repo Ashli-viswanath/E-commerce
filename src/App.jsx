f751231344e0a47cc956509806cf28e78ee3d0d9
Ashli Viswanath <64769138+Ashli-viswanath@users.noreply.github.com>
2024-05-14 15:17:29 +0530

    implement functionality of cart buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { commerce } from "./lib/commerce";
 import Cart from "./components/Cart/Cart";
-import {Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Products from "./components/Products/Products";
 function App() {
   const [products, setProducts] = useState([]);
@@ -17,10 +17,21 @@ function App() {
     setCart(await commerce.cart.retrieve());
   };
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
-    setCart(item.cart);
+    const { cart } = await commerce.cart.add(productId, quantity);
+    setCart(cart);
   };
-
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+    setCart(cart);
+  };
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+    setCart(cart);
+  };
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
+  };
   useEffect(() => {
     fetchProducts();
     fetchCart();
@@ -31,8 +42,25 @@ function App() {
       <div>
         <Navbar totalItems={cart.total_items} />
         <Routes>
-          <Route exact path="/" element={ <Products products={products} onAddToCart={handleAddToCart} />}/>
-          <Route exact path="/cart" element={<Cart cart={cart} />}/>
+          <Route
+            exact
+            path="/"
+            element={
+              <Products products={products} onAddToCart={handleAddToCart} />
+            }
+          />
+          <Route
+            exact
+            path="/cart"
+            element={
+              <Cart
+                cart={cart}
+                handleUpdateCartQty={handleUpdateCartQty}
+                handleRemoveFromCart={handleRemoveFromCart}
+                handleEmptyCart={handleEmptyCart}
+              />
+            }
+          />
         </Routes>
       </div>
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,12 +6,18 @@ import useStyles from "./styles";
 import { Link, useLocation } from "react-router-dom";
 const Navbar = ({ totalItems }) => {
   const classes = useStyles();
-  const location=useLocation();
+  const location = useLocation();
   return (
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
-          <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
             <img
               src={logo}
               alt="Commerce.js"
@@ -23,16 +29,19 @@ const Navbar = ({ totalItems }) => {
           <div className={classes.grow} />
 
           {location.pathname === "/" && (
-            
-  
-          <div className={classes.button}>
-            <IconButton component={Link} to="/cart"  aria-label="Show cart items" color="inherit">
-              <Badge badgeContent={totalItems} color="secondary">
-                <ShoppingCartIcon />
-              </Badge>
-            </IconButton>
-          </div>
-                  )}
+            <div className={classes.button}>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCartIcon />
+                </Badge>
+              </IconButton>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </>
